6d47a90bb2c78367f1b1d05268c07db6f5d7deb6
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2022-06-13 13:38:29 -0400

    I added more sounds, but I'm wondering if they should be longer, because I find them a bit short and it adds to the robotic feeling. They could be longer and each key could be punctuated by a short breathing sound. I removed all the breathing sounds because I felt like the moaning with the breathing was off. The thing is I want to sounds to be similar but not the same, to create an organic feel to it.

--- experiment_1/script.js
+++ experiment_1/script.js
@@ -3,14 +3,16 @@
 const audio = new Audio("assets/sounds/man_moan_1.wav")
 audio.volume = 0.1;
 let sounds = [];
-  const sources = ['assets/sounds/man_moan_1.wav', 'assets/sounds/man_moan_2.wav', 'assets/sounds/man_moan_3.wav'];
+  const sources = ['assets/sounds/man_moan_4.wav', 'assets/sounds/man_moan_7.wav', 'assets/sounds/man_moan_8.wav', 'assets/sounds/man_moan_10.wav', 'assets/sounds/man_moan_11.wav', 'assets/sounds/man_moan_12.wav', 'assets/sounds/man_moan_13.wav', 'assets/sounds/man_moan_14.wav'];
+  // const sources = ['assets/sounds/man_moan_1.wav', 'assets/sounds/man_moan_2.wav', 'assets/sounds/man_moan_3.wav', 'assets/sounds/man_moan_4.wav', 'assets/sounds/man_moan_5.wav', 'assets/sounds/man_moan_6.wav', 'assets/sounds/man_moan_7.wav', 'assets/sounds/man_moan_8.wav', 'assets/sounds/man_moan_9.wav', 'assets/sounds/man_moan_10.wav', 'assets/sounds/man_moan_11.wav', 'assets/sounds/man_moan_12.wav', 'assets/sounds/man_moan_13.wav', 'assets/sounds/man_moan_14.wav'];
+
   for (let i = 0; i < sources.length; i++) {
       sounds[i] = new Audio(sources[i]);
   }
 
 
 function playRandomSound() {
-    const randomNumber = Math.floor(Math.random() * 3);
+    const randomNumber = Math.floor(Math.random() * 8);
 
     sounds[randomNumber].play();
     sounds[randomNumber].volume = 0.1;
@@ -35,6 +37,22 @@ buttons: {
 });
 
 $( "#name" ).keypress(function() {
+  // console.log('input')
+  $( "#dialog-form" ).effect("bounce", {times:3, distance: 10, times: 3}, 200);
+  $( "body" ).effect('highlight',{color: 'pink'}, 200);
+  // audio.play();
+playRandomSound();
+});
+
+$( "#email" ).keypress(function() {
+  // console.log('input')
+  $( "#dialog-form" ).effect("bounce", {times:3, distance: 10, times: 3}, 200);
+  $( "body" ).effect('highlight',{color: 'pink'}, 200);
+  // audio.play();
+playRandomSound();
+});
+
+$( "#password" ).keypress(function() {
   // console.log('input')
   $( "#dialog-form" ).effect("bounce", {times:3, distance: 10, times: 3}, 200);
   $( "body" ).effect('highlight',{color: 'pink'}, 200);
