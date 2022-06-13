e7bde04b1f7c854249fca0548422ec6631257406
miamiv1ce <49047032+miamiv1ce@users.noreply.github.com>
2022-06-13 12:50:11 -0400

    I added 3 moaning sounds, I chose male sounding sounds because It made me laugh more and it's less cliché. Although I want to make it so that it's not so gendered, so I might try to mix both together. I tried with sexy music and it adds something, royalty free erotic jazz. I'm not too sure about it though. One thing i'm struggling with is how to make it more human. Since theres only 3 sounds now It sounds robotic and I really want to make it feel more human. I wonder if i should add other sound effects around. Like bed cracking or something. I don't think. I thought of keyboard sounds? But it's too joke-y. Anyway something is missing in terms of making it sound more human, i'll try to add more sounds.

--- experiment_1/script.js
+++ experiment_1/script.js
@@ -1,23 +1,43 @@
 "use strict";
 
+const audio = new Audio("assets/sounds/man_moan_1.wav")
+audio.volume = 0.1;
+let sounds = [];
+  const sources = ['assets/sounds/man_moan_1.wav', 'assets/sounds/man_moan_2.wav', 'assets/sounds/man_moan_3.wav'];
+  for (let i = 0; i < sources.length; i++) {
+      sounds[i] = new Audio(sources[i]);
+  }
+
+
+function playRandomSound() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    sounds[randomNumber].play();
+    sounds[randomNumber].volume = 0.1;
+
+}
 
 $("form").attr('autocomplete', 'off');
 
+$('document').ready(function() {
+  });
+
 $("#dialog-form").dialog({
-modal: true,
+modal: false,
 buttons: {
   "Log in": function() {
     // $(this).dialog("close");
     $( this ).toggle("explose");
 
-    console.log('close')
+    // console.log('close')
   }
 }
 });
 
 $( "#name" ).keypress(function() {
-  console.log('input')
+  // console.log('input')
   $( "#dialog-form" ).effect("bounce", {times:3, distance: 10, times: 3}, 200);
   $( "body" ).effect('highlight',{color: 'pink'}, 200);
-
+  // audio.play();
+playRandomSound();
 });
